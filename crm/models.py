0b276a0122702d41593313fed7efeebf1bc3ef23
winniekathomi <kathomimemba@gmail.com>
2025-06-27 10:54:18 +0300

    Task 0 & 1: Setup Django project, created CRM models and queries

---
+++ crm/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    order_date = models.DateTimeField(auto_now_add=True)
+ # Create your models here.
+
